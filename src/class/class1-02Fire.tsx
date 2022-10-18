a392ec81270b24e07250f8f1d7b05981a3d5e1a0
luis <luiszkm@gmail.com>
2022-10-18 10:02:11 -0300

    finished calls2Fire

--- src/class/class1-02Fire.tsx
+++ src/class/class1-02Fire.tsx
@@ -7,6 +7,9 @@ import conducaoCalor from "../assets/img01/conducaoCalor.webp"
 import conveccao from "../assets/img01/conveccao.png"
 import radiacao from "../assets/img01/radiacao.png"
 import classeA from "../assets/img01/classeA.png"
+import classeB from "../assets/img01/classeB.png"
+import classeC from "../assets/img01/classeC.png"
+import classeD from "../assets/img01/classeD.png"
 
 
 
@@ -88,23 +91,68 @@ export function Class1Fire() {
         Nessa forma o calor se propaga através das ondas."
         img={radiacao}
       />
-      
-      <h4>Classes de Incêndio</h4>
-        ▪ Classe A
-        ▪ Classe B
-        ▪ Classe C
-        ▪ Classe D
 
-        <SectionItem withH4
+      <h4>Classes de Incêndio</h4>
+      ▪ Classe A
+      ▪ Classe B
+      ▪ Classe C
+      ▪ Classe D
+
+      <SectionItem withH4
         title="Incêndio de Classe A"
         paragraph={`São aqueles ocorridos em materiais sólidos. 
         Queimam em razão da área (superfície) e	da profundidade.
         Deixam resíduos.
         Papel, madeira, etc.`}
-        
         img={classeA}
       />
 
+      <SectionItem withH4
+        title="Incêndio de Classe B"
+        paragraph="São aqueles que ocorrem em líquidos, pastosos e/ou gases inflamáveis.
+         Queimam em razão da área.Não deixam resíduos.Ex: Gasolina, GLP etc."
+        img={classeB}
+      />
+
+      <SectionItem withH4
+        title="Incêndio de Classe C"
+        paragraph="São os que envolvem aparelhos elétricos energizados. 
+        Devendo	sempre desligar a fonte de energia elétrica.
+	      Alto risco de vida.Fios energizados, Tomadas etc."
+        img={classeC}
+      />
+
+      <SectionItem withH4
+        title="Incêndio de Classe D"
+        paragraph="São	aqueles	que	ocorrem	em	metais pirofóricos.
+        Para sua extinção devem ser usados agentes extintores especiais.
+        Incêndios em	ligas de magnésio, selênio etc."
+        img={classeD}
+      />
+
+      <h4>Processões e Extinção de incêndio</h4>
+      ▪ Resfriamento
+      ▪ Abafamento
+      ▪ Retirada de material
+      ▪ Extinção Química
+      
+      <SectionItem 
+        title="Resfriamento:"
+        paragraph="Consiste	na	retirada	de	calor do combustível incendiado.
+        O melhor agente de absorção do calor é a água."
+      />
+      <SectionItem 
+        title="Abafamento:"
+        paragraph="Consiste na extinção de um incêndio pela diminuição do oxigênio."
+      />
+      <SectionItem 
+        title="Retirada de Material:"
+        paragraph="Consiste	na	diminuição	do	campo	de propagação do	incêndio."
+      />
+      <SectionItem 
+        title="Quebra da reação:"
+        paragraph="Consiste em interromper a reação química através de agentes químicos especiais."
+      />
     </>
   )
 }
---
+++ src/class/class1-03Extintor.tsx
@@ -0,0 +1,25 @@
+import { List } from "../components/List";
+import { SectionItem } from "../components/SectionItem";
+import { FaFireExtinguisher } from "react-icons/fa"
+
+import trianguloFogo from "../assets/img01/trianguloFogo.png"
+import conducaoCalor from "../assets/img01/conducaoCalor.webp"
+import conveccao from "../assets/img01/conveccao.png"
+import radiacao from "../assets/img01/radiacao.png"
+import classeA from "../assets/img01/classeA.png"
+import classeB from "../assets/img01/classeB.png"
+import classeC from "../assets/img01/classeC.png"
+import classeD from "../assets/img01/classeD.png"
+
+
+
+export function class1Extintor() {
+
+  return (
+    <>
+      <h3>MÉTODOS DE EXTINÇÃO DE INCÊNDIOS<FaFireExtinguisher size={24} /></h3>
+
+   
+    </>
+  )
+}
