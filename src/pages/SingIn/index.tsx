1ec0191e1eb308c41edf844e15b69fe9a6da2b31
luis <luiszkm@gmail.com>
2022-10-26 11:29:39 -0300

    text

--- src/pages/SingIn/index.tsx
+++ src/pages/SingIn/index.tsx
@@ -1,5 +1,5 @@
 import { Button } from "../../components/Button";
-import { Input} from "../../components/Input"
+import { Input} from "../../components/Input";
 import { AiOutlineMail, AiOutlineUnlock } from "react-icons/ai"
 import { Container, FormLogin } from "./styles";
 import { Link } from "react-router-dom";
@@ -19,17 +19,13 @@ export function SingIn() {
         {<FormLogin >
           <label htmlFor="email">
             Digite seu e-mail
-            <Input id="email"
-              type="email"
-              placeholder="brigada@exemplo.com"
+            <Input
               icon={<AiOutlineMail />} />
           </label>
 
           <label htmlFor="password">
             Digite sua senha
-            <Input id="password"
-              type="email"
-              placeholder="mínimo 6 carácter"
+            <Input
               icon={<AiOutlineUnlock />} />
           </label>
           
