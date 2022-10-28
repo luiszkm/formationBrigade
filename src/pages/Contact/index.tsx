10a4f0b53e2a383622ff1d6e817733fcd2ef1143
luis <luiszkm@gmail.com>
2022-10-28 15:49:55 -0300

    add page contact

--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -1,14 +1,23 @@
 import { Footer } from "../../components/Footer";
 import { Header } from "../../components/Header";
+import { Container } from "./styles";
 
-
+import contact from "../../assets/contact.svg"
+import { AiOutlineMail, AiOutlineWhatsApp } from "react-icons/ai";
 
 export function Contact (){
 
   return(
-    <>
+    <Container>
     <Header />
+    <div>
+      <ul>
+        <li><AiOutlineWhatsApp /> (31) 98710-2181</li>
+        <li><AiOutlineMail /> mrenildo@yahoo.com.br</li>
+      </ul>
+    </div>
+    <img src={contact} alt="" />
     <Footer />
-    </>
+    </Container>
   )
 }
---
+++ src/pages/Contact/styles.ts
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+
+
+
+export const  Container = styled.div`
+
+min-height:100vh;
+display: flex;
+flex-direction: column;
+justify-content: space-between;
+
+>div:nth-child(2){
+  width:100% ;
+  height: 30vh;
+  background-image: linear-gradient(0deg, rgba(255,0,0,0), rgba(255,0,0,1),rgba(255,0,0,0));
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  font-weight:bold;
+
+  >ul{
+    display: flex;
+    flex-direction: column;
+    gap: 12px;
+
+    >li{
+      display: flex;
+      align-items: center;
+      gap: 8px;
+    }
+  }
+
+
+  }
+`
+  
+
+
+
+
